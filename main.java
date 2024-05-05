79b5193489a1701137fd964f2d4ed67786e5969e
Your Name <you@example.com>
2024-05-05 19:51:56 +0900

    게시판

--- main.java
+++ main.java
@@ -81,6 +81,20 @@ public class main {
                 System.out.println("== 게시판을 종료합니다 ==");
                 break;
             }
-        } 
-    }
+        }
+}
 
+class Article {
+  int id;
+  String title;
+  String body;
+  Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+  @Override // 어노테이션
+  public String toString() {
+    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
+  }
+} 
